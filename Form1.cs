7b2446de34c81ef0879cb2ebe66242662f84ec29
George Pecherle <george.pecherle@gmail.com>
2024-10-07 09:29:24 +0300

    add files

--- Form1.cs
+++ Form1.cs
@@ -2,16 +2,36 @@ namespace PrimulMeuCursDeGrafica
 {
     public partial class Form1 : Form
     {
+        MyGraphics myGraphics;
+        float a = 0, b = 0;
         public Form1()
         {
             InitializeComponent();
+            myGraphics = new MyGraphics(pictureBox1);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // DrawPolygon(myGraphics.grp, RegularPolygon(4, new PointF(250, 250), 150, 0));
+
+            /*
+            for (float a=0; a<=(float)(Math.PI * 2); a += 0.01f)
+            {
+                DrawPolygon(myGraphics.grp, RegularPolygon(4, new PointF(250, 250), 150, a));
+            }
+
+            float b = 0;
+            for (float a = 0; a <= 200; a += 10, b += 0.1f)
+            {
+                DrawPoints(myGraphics.grp, RegularPolygon(6, new PointF(250, 250), a, b));
+
+            }
+            */
+            button1.Text = "Start";
+            myGraphics.Refresh();
         }
 
-        private static List<PointF> Pr(int n, PointF C, float R, float fi)
+        private static List<PointF> RegularPolygon(int n, PointF C, float R, float fi)
         {
             List<PointF> toReturn = new List<PointF>();
 
@@ -24,6 +44,37 @@ namespace PrimulMeuCursDeGrafica
                 toReturn.Add(new PointF(x, y));
             }
             return toReturn;
+        }
+
+        public void DrawPolygon(Graphics grp, List<PointF> points)
+        {
+            grp.DrawPolygon(new Pen(Color.Black), points.ToArray());
+        }
+
+        public void DrawPoints(Graphics grp, List<PointF> points)
+        {
+            foreach(var point in points)
+            {
+                grp.DrawEllipse(new Pen(Color.Black), point.X - 1, point.Y - 1, 3, 3);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            a += 10;
+            b += 0.1f;
+
+            if (a >= 200)
+                a = 10;
+
+            myGraphics.Clear();
+            DrawPoints(myGraphics.grp, RegularPolygon(6, new PointF(250, 250), a, b));
+            myGraphics.Refresh();
         }
     }
 }
